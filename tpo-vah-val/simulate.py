f1199c8677dd58c0c73d2e0f4b7abddd379101e7
Jannic <jannic.marcon@hotmail.com>
2024-04-03 20:04:51 +0200

    analysis modified

--- tpo-vah-val/simulate.py
+++ tpo-vah-val/simulate.py
@@ -11,13 +11,20 @@ def analyze_strategy(trades):
     num_trades = len(trades)
     num_poc = 0
     num_range = 0
+    plus = 0
+    minus = 0
     for trade in trades:
         if trade.poc_hit is not None:
+            plus += abs(trade.entry - trade.poc_hit)
             num_poc += 1
         if trade.range_hit is not None:
             num_range += 1
 
-    print(f'Trades: {num_trades}; POC Hits: {num_poc}; Range Hits: {num_range}')
+    loss_trades = num_trades - num_poc
+
+    print(f'Trades: {num_trades}; POC Hits: {num_poc}; Range Hits: {num_range}; Loss Trades: {loss_trades}')
+    print(f'Profit: {plus}')
+
 
 
 def simulate_strategy(data):
@@ -40,7 +47,6 @@ def simulate_strategy(data):
         if current_time == time(hour=23, minute=30):
             current_time = time(hour=6)
             daily_date += timedelta(days=1)
-            poc, vah, val = (0, 0, 0)
 
         boolean_series: pd.Series = (data['timestamp'].dt.date == daily_date.date()) & (data['timestamp'].dt.time <= current_time)
         daily_data: pd.DataFrame = data[boolean_series]
@@ -48,23 +54,19 @@ def simulate_strategy(data):
         if len(daily_data) == 0:
             break
 
-        if poc > 0:
-            #in trade
-            if isinstance(trade, trade_model):
-                trade, trade_finished = handle_trade(daily_data, trade)
+        #in trade
+        if isinstance(trade, trade_model):
+            trade, trade_finished = handle_trade(daily_data, trade)
 
-            #entry
-            if trade_finished:
-                trade_finished = False
-                all_trades.append(trade)
-                trade = None
-                
-            if trade is None:
-                trade = enter_logic(daily_data, data)
+        #entry
+        if trade_finished:
+            trade_finished = False
+            all_trades.append(trade)
+            trade = None
+            
+        if trade is None:
+            trade = enter_logic(daily_data, data)
 
-
-
-        poc, vah, val = create_tpo(daily_data)
 
         current_time = add_minutes(current_time)
 
