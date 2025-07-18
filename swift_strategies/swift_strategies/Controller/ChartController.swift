3e4ecaee4b4273f5a58470839d459b5fb3a33eff
Jannic <jannic.marcon@hotmail.com>
2025-07-18 21:16:56 +0200

    move start backtest & validate chart method

--- swift_strategies/swift_strategies/Controller/ChartController.swift
+++ swift_strategies/swift_strategies/Controller/ChartController.swift
@@ -7,6 +7,28 @@
 import Foundation
 
 struct ChartController {
+    private func validateChart(chart: [Candle]) -> Bool {
+        //check for gaps in the candles
+        var firstTimeGap: Int = 0
+        var lastTime: Int = 0
+        for candle in chart {
+            if firstTimeGap > 0 {
+                if lastTime + firstTimeGap != candle.time {
+                    return false
+                }
+            }
+            else{
+                if lastTime > 0{
+                    firstTimeGap = candle.time - lastTime
+                }
+            }
+            lastTime = candle.time
+        }
+        
+        return true
+    }
+    
+    
     private func getChartWithIndicaors(filepaths: [URL], indicatorController: IndicatorController) -> [IndicatorCandle] {
         var candles: [Candle] = []
         for chartPart in filepaths {
@@ -14,12 +36,14 @@ struct ChartController {
             candles.append(contentsOf: part)
         }
         candles.sort { $0.time < $1.time }
+        assert(validateChart(chart: candles), "There is a gap in the chart")
         
         return indicatorController.addIndicators(candles: candles, "sma200", "sma20", "sma5", "atr")
     }
     
+    
     public func getAllChartsWithIndicaors() async -> [String: [IndicatorCandle]] {
-        let allChartPaths = CsvController.getAllCharts().filter { $0.key != "bak" }
+        let allChartPaths = CsvController.getAllCharts().filter { $0.key != "bak" && $0.key != "tmp"  }
         var allCharts: [String: [IndicatorCandle]] = [:]
         
         let indicatorController = IndicatorController()
--- swift_strategies/swift_strategies/StartingPoint.swift
+++ swift_strategies/swift_strategies/StartingPoint.swift
@@ -10,51 +10,7 @@ import Foundation
 @main
 struct StartingPoint {
     static func main() async throws {
-        let backtestingStrat: Strategy = TrippleEmaStrategy()
-        let evaluationController = EvaluationController()
-        let chartController = ChartController()
-        
-        let allCharts = await chartController.getAllChartsWithIndicaors()
-        
-        var parameterSets: [(chart: String, tpMult: Double, slMult: Double)] = []
-
-        for (chartName, _) in allCharts {
-            for tpMult in stride(from: 3.0, through: 9.0, by: 0.5) {
-                for slMult in stride(from: 2.0, through: 6.0, by: 0.5) {
-                    parameterSets.append((chartName, tpMult, slMult))
-                }
-            }
-        }
-
-        let batchSize = 50
-        let batches = parameterSets.chunked(into: batchSize)
-        
-        var batchIndex = 0
-        var allEvaluations: [EvaluationModel] = []
-
-        for batch in batches {
-            batchIndex += 1
-            await withTaskGroup(of: EvaluationModel?.self) { group in
-                for (chartName, tpMult, slMult) in batch {
-                    group.addTask {
-                        var eval = backtestingStrat.backtest(chart: allCharts[chartName]!, tpMult: tpMult, slMult: slMult)
-                        eval.origin = chartName + "\n tpMult: \(tpMult), slMult: \(slMult)"
-                        return eval
-                    }
-                }
-
-                for await eval in group {
-                    if let eval = eval {
-                        allEvaluations.append(eval)
-                    }
-                }
-            }
-            print("batch \(batchIndex)/\(batches.count) done")
-        }
-
-        allEvaluations.sort(by: { $0.averageRMultiples > $1.averageRMultiples })
-        
-        JsonController.saveEvaluationsToJson(objects: allEvaluations, filename: "/Users/jannicmarcon/Documents/Other/evaluations_1.json")
-        evaluationController.evaluateEvaluations(evaluations: allEvaluations)
+        let backtestController = BacktestController()
+        await backtestController.runBacktest()
     }
 }
