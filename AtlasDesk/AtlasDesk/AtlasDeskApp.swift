f0925bd92eaf00e72cbb79c8a3f2e16e0e2436df
Jannic <jannic.marcon@hotmail.com>
2025-08-22 21:13:05 +0200

    create package structure

---
+++ AtlasDesk/AtlasDesk/AtlasDeskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AtlasDeskApp.swift
+//  AtlasDesk
+//
+//  Created by Jannic Marcon on 22.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct AtlasDeskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
